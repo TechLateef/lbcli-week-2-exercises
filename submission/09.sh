9aa3d972892fe925b57975beaf46b131dc6c5578
techlateef <techlateef@gmail.com>
2026-03-23 20:54:31 +0100

    fix: json formating

--- submission/09.sh
+++ submission/09.sh
@@ -6,4 +6,4 @@ raw_tx="01000000000101c8b0928edebbec5e698d5f86d0474595d9f6a5b2e4e3772cd9d1005f23
 recipient=2MvLcssW49n9atmksjwg2ZCMsEMsoj3pzUP
 change_addr=$(bitcoin-cli -regtest getnewaddress)
 txid=$(bitcoin-cli -regtest decoderawtransaction $raw_tx | jq -r '.txid')
-bitcoin-cli -regtest createrawtransaction "[{"txid": "$txid", "vout":0, "sequence":1},{"txid": "$txid", "vout":1, "sequence":1}]" "{"$recipient": 0.2, "$change_addr": 0.03659108}"
+bitcoin-cli -regtest createrawtransaction "[{\"txid\":\"$txid\",\"vout\":0,\"sequence\":1},{\"txid\":\"$txid\",\"vout\":1,\"sequence\":1}]" "{\"$recipient\":0.2,\"$change_addr\":0.03659108}"
